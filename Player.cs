4d1a1dce3edc0b955fab21465973aa65781a7397
MTGz17 <150878018+MTGz17@users.noreply.github.com>
2024-03-03 21:11:54 -0500

    Current Scripts

--- Player.cs
+++ Player.cs
@@ -1,17 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public float speed = 3.0f; //Movement speed, faster than enemy
-    public int health = 100; //Health, has room to take damage
+    public int health = 3; //Health, has room to take damage
+    public int maxHealth;
+
+    public Sprite emptyHeart;
+    public Sprite fullHeart;
+    public Image[] hearts;
+
+    public Camera cam;
+
+    void Start()
+    {
+
+    }
 
     void Update()
     {
        float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime; //Horizontal input
        float moveZ = Input.GetAxis("Vertical") * speed * Time.deltaTime; //Vertical input
        transform.position += new Vector3(moveX, 0, moveZ); //Movement
+
+        //Camera looking at the cursdor
+        Vector3 point = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+        float t = cam.transform.position.y / (cam.transform.position.y - point.y);
+        Vector3 finalPoint = new Vector3(t * (point.x - cam.transform.position.x) + cam.transform.position.x, 1, t * (point.z - cam.transform.position.z) + cam.transform.position.z);
+        transform.LookAt(finalPoint, Vector3.up);
+
+
+        //Health UI
+        for(int i = 0; i < hearts.Length; i++)
+        {
+            if(i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+            if(i < maxHealth)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+
     }
     
     public void Damage(int damage) //Damage method by subtracting damage from health
--- Week7Plane.cs
+++ Week7Plane.cs
@@ -6,13 +6,13 @@ public class Week7Plane : MonoBehaviour
 {
 
     public float length, height;
-
+    [SerializeField] private GameObject prefab;
 
     void Start()
     {
 
         CreateGround();
-        
+        InvokeRepeating("SpawnObject", 1, 1.5f);
     }
 
     void CreateGround()
@@ -23,6 +23,35 @@ public class Week7Plane : MonoBehaviour
         plane.transform.localScale = new Vector3 (length, 1, height);
         var planeRenderer = plane.GetComponent<Renderer>();
         planeRenderer.material.SetColor("_Color", Color.black);
+
+    }
+
+    public void SpawnObject()
+    {  
+        GameObject prefabGameObject = Instantiate(prefab);
+        
+        int generator;
+        generator = Random.Range(0, 4);
+
+        float edge;
+        edge = length * 5;
+
+        if(generator == 0)
+        {
+            prefabGameObject.transform.position = new Vector3(Random.Range(-25f,25f), .5f, -edge);
+        }
+        if(generator == 1)
+        {
+            prefabGameObject.transform.position = new Vector3(Random.Range(-25f,25f), .5f, edge);
+        }
+        if(generator == 2)
+        {
+            prefabGameObject.transform.position = new Vector3(-edge, .5f, Random.Range(-25f,25f));
+        }
+        if(generator == 3)
+        {
+            prefabGameObject.transform.position = new Vector3(edge, .5f, Random.Range(-25f,25f));
+        }
 
     }
 
